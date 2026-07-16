df5f5e3d498dbcbf48d63ac86b32d3c501ebda57
Elias Gabriel Huber <huber.eliasgabriel@gmail.com>
2026-07-16 20:22:49 +0200

    Test & Termin

--- scripts/events.js
+++ scripts/events.js
@@ -12,5 +12,12 @@ const events = [
     time: '18:00',
     location: 'Amalia Pernter',
     link: ''
-  }
+  },
+  {
+    title: '"Neapolitanisch aufgspielt" - Innsbrucker Festwochen der Alten Musik',
+    date: '2026-08-18',
+    time: '20:00',
+    location: 'Tiroler Landestheater Innsbruck',
+    link: 'https://www.altemusik.at/de/veranstaltungen/neapolitanisch-aufgspielt'
+  },
 ];
